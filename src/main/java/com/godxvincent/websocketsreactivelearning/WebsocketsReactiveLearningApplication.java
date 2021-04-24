f276de529b9871a822cc3af2fc963c22065dbe86
godxvincent <godxvincent@gmail.com>
2021-04-24 14:50:55 -0500

    :construction: Initial Commit

---
+++ src/main/java/com/godxvincent/websocketsreactivelearning/WebsocketsReactiveLearningApplication.java
@@ -0,0 +1,13 @@
+package com.godxvincent.websocketsreactivelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsReactiveLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsReactiveLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godxvincent/websocketsreactivelearning/WebsocketsReactiveLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godxvincent.websocketsreactivelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsReactiveLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
